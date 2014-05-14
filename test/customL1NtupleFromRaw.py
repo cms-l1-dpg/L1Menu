1a405af116d460e9c6ed63468a07a1d43bedd1f4
Leonard Apanasevich <apana@fnal.gov>
2014-05-14 11:39:31 +0200

    bug fix

--- test/customL1NtupleFromRaw.py
+++ test/customL1NtupleFromRaw.py
@@ -9,13 +9,12 @@ if options.useStage1Layer2:
     process.p *= process.Layer2
     process.p *= process.l1NtupleProducerStage1Layer2
 
-process.TFileService.fileName=OUTFILE
-
 # edit here
 
-OUTFILE="L1Tree.root"
+OUTFILE="L1Tree_both.root"
 NEVTS=-1
 
+process.TFileService.fileName=OUTFILE
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(NEVTS) )
 
 ## readFiles.extend( ['file:///data2/battilan/L1Trigger/62X_RAW_RECO.root'] )
