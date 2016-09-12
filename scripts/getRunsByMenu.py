7f25d166fcbf9b582243fc56dbec2dc598d4e2eb
Takashi MATSUSHITA <takashi.matsushita@cern.ch>
2016-09-12 16:50:43 +0200

    changed query to include menu name in the result

--- scripts/getRunsByMenu.py
+++ scripts/getRunsByMenu.py
@@ -30,13 +30,17 @@ if __name__ == '__main__':
   con = cx_Oracle.connect('%s/%s@%s' % (options.user, options.passwd, options.database))
   cur = con.cursor()
   
-  query = """select lhcfill, runnumber, starttime, gtkey, tsckey from cms_wbm.runsummary where tsckey in (select id from cms_trg_l1_conf.l1_trg_conf_keys where id like 'collisions' || '%%' and ugt_key in (select id from cms_trg_l1_conf.ugt_keys where ugt_keys.l1_menu like '%s' || '%%')) order by runnumber""" % options.menu
+  query = """select wbm.lhcfill, wbm.runnumber, wbm.gtkey, conf.id, menu.l1_menu
+             from cms_wbm.runsummary wbm
+             join cms_trg_l1_conf.l1_trg_conf_keys conf on wbm.tsckey in conf.id
+             join cms_trg_l1_conf.ugt_keys menu on (conf.ugt_key in menu.id)
+             where conf.id like 'collisions' || '%%' and menu.l1_menu like '%s' || '%%' order by wbm.runnumber""" % options.menu
   cur.execute(query)
   rc = cur.fetchall()
   print 'Run # for %s' % options.menu
   for x in rc:
     if x[0]:
-      print '  ', x[1], 'started at ', x[2], ' with keys: ', x[3], ' ', x[4]
+      print '  ', x[1], 'with menu:', x[4], " (", x[2], ",", x[3], ")"
   print
 
 # eof
