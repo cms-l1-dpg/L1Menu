bd6ca0da7f7578ddda466c3ff39e9f9674ed5bba
Zhenbin Wu <zhenbin.wu@gmail.com>
2017-03-23 12:35:07 -0500

    Add l1CaloTower to menulib

--- macros/L1Menu2016.C
+++ macros/L1Menu2016.C
@@ -1099,7 +1099,7 @@ bool L1Menu2016::BuildRelation()
 bool L1Menu2016::L1SeedFunc()
 {
 #ifdef UTM_MENULIB
-  addFuncFromName(L1SeedFun, upgrade_);
+  addFuncFromName(L1SeedFun, upgrade_, l1CaloTower_);
 #endif
     
   for(auto &L1Seed : mL1Seed)
