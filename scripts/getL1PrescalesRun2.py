f20343eb6c393b522e7e7a3e3a852711583037e4
Takashi MATSUSHITA <takashi.matsushita@cern.ch>
2017-06-08 15:11:44 +0200

    generate xml and csv files

--- scripts/getL1PrescalesRun2.py
+++ scripts/getL1PrescalesRun2.py
@@ -9,6 +9,118 @@
 import argparse
 import cx_Oracle
 
+MAX_INDEX = 512
+
+
+def mask_file(run, menu, mask):
+  rows = ""
+  for ii in range(MAX_INDEX):
+    if ii in mask:
+      rows += "        <row>%d,%d</row> <!-- %s -->\n" % (ii, mask[ii], menu[ii])
+    else:
+      rows += "        <row>%d,%d</row>\n" % (ii, 0)
+  rows = rows.strip('\n')
+
+  template = """<run-settings id="uGT">
+  <context id="uGtProcessor">
+    <param id="finorMask" type="table">
+      <columns>
+ 	      algo,mask
+      </columns>
+      <types>
+ 	      uint,uint
+      </types>
+      <rows>
+%s
+      </rows>
+    </param>
+  </context>
+</run-settings>
+"""
+
+  output = file('mask-run%d.xml' % run, 'w')
+  output.write(template % rows)
+  output.close()
+
+  return
+
+
+def prescale_file(run, menu, prescales):
+  header = "      <columns>algo/prescale-index"
+  types = "      <types>uint"
+  for ii in range(len(prescales[prescales.keys()[0]].keys())):
+    header += ", %d" % ii
+    types += ", uint"
+  header += "</columns>"
+  types += "</types>"
+
+  rows = ""
+  for ii in prescales:
+    if ii in menu:
+      rows += "        <row>%d" % ii
+      for key in prescales[ii]:
+        ps = prescales[ii][key]
+        rows += ",%d" % ps
+      rows += "</row> <!-- %s -->\n" % menu[ii]
+  rows = rows.strip('\n')
+
+  prescale = {}
+  prescale['header'] = header
+  prescale['rows'] = rows
+  prescale['types'] = types
+
+  template = """<?xml version="1.0" encoding="UTF-8"?>
+<run-settings id="uGT">
+  <context id="uGtProcessor">
+    <param id="index" type="uint">0</param>
+    <param id="prescales" type="table">
+%(header)s
+%(types)s
+      <rows>
+%(rows)s
+      </rows>
+    </param>
+  </context>
+</run-settings>
+"""
+
+  output = file('prescale-run%d.xml' % run, 'w')
+  output.write(template % prescale)
+  output.close()
+
+  return
+
+
+def csv_file(run, menu, prescales):
+  header = "id,name,proposed name"
+  for ii in range(len(prescales[prescales.keys()[0]].keys())):
+    header += ",%d" % ii
+  header += ",xml,POG,PAG,comment"
+
+  rows = ""
+  for ii in prescales:
+    if ii in menu:
+      rows += "%d,%s," % (ii, menu[ii])
+      for key in prescales[ii]:
+        ps = prescales[ii][key]
+        rows += ",%d" % ps
+      rows += ",,,,\n"
+  rows = rows.strip('\n')
+
+  csv = {}
+  csv['header'] = header
+  csv['rows'] = rows
+
+  template = """%(header)s
+%(rows)s
+"""
+
+  output = file('run%d.csv' % run, 'w')
+  output.write(template % csv)
+  output.close()
+
+  return
+
 
 if __name__ == '__main__':
   run = None
@@ -50,14 +162,20 @@ if __name__ == '__main__':
     query = """select prescale_index, prescale from cms_ugt_mon.view_ugt_run_prescale where algo_index = %s and run_number = %s order by prescale_index""" % (idx, options.run)
     cur.execute(query)
     rc = cur.fetchall()
-    prescales = {}
+    row = {}
     for x in rc:
-      prescales[x[0]] = x[1]
+      row[x[0]] = x[1]
       if options.apply_mask:
-        prescales[x[0]] = x[1] * mask[idx]
+        row[x[0]] = x[1] * mask[idx]
 
-    for k, v in prescales.iteritems():
+    for k, v in row.iteritems():
       print "%6d" % v,
     print
+
+    prescales[idx] = row
+
+  mask_file(options.run, menu, mask)
+  prescale_file(options.run, menu, prescales)
+  csv_file(options.run, menu, prescales)
 
 # eof
